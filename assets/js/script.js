ce7c5c45437b938f16fb6a46e0eec2d4362828f2
sylvynhaarc@hotmail.com <you@example.com>
2022-11-08 10:40:02 -0600

    The function of a color-code event implemented, and read me file update

---
+++ assets/js/script.js
@@ -0,0 +1,74 @@
+
+const viewcurrentDay = document.querySelector('#currentDay');
+const currentday= moment();
+viewcurrentDay.textContent = currentday.format('LLL');
+var currentHour = parseInt(moment().format("H"));
+
+
+let timeBlock = $(".time-block").addClass('row');
+let description= $('.description').addClass('description');
+
+//Saving Event
+
+    // On button click, get value
+    
+const savedevent= JSON.parse(localStorage.getItem('savedevent')) || [];
+$(document).ready(function () {
+    
+    $(".saveBtn").click(function (event) {
+        event.preventDefault()
+
+        var currentRow=$(this).closest("tr"); 
+         
+         const data ={
+          description: currentRow.find("td:eq(0) input[type='text']").val(),
+           time: currentRow.find("th:eq(0)").html(),
+        
+        }
+    
+        savedevent.push(data)
+        localStorage.setItem('savedevent',JSON.stringify(savedevent));
+        
+    });
+
+});
+//localStorage.clear();
+
+$( window ).on( "load", function() {
+   
+     
+    var events = JSON.parse(localStorage.getItem("savedevent"));
+     $.each(events,function (i,val){
+        var desc= $('<input desc="'+ val.description + '" />');
+           $('.description').append(desc);
+
+        // $('.description')= desc
+     });
+});
+       
+   
+$(".description").each(function () {
+    var $this = $(this);
+    var id = parseInt($this.attr("id"));
+  
+  if (id < currentHour){
+  $(this).addClass("past");
+  $(this).removeClass("present")
+  $(this).removeClass("future")
+  }
+  
+  if (id > currentHour){
+  $(this).removeClass("past");
+  $(this).removeClass("present")
+  $(this).addClass("future")
+  }
+  
+  else {
+    $(this).removeClass("past");
+  $(this).addClass("present")
+  $(this).removeClass("future")
+  }
+  
+  
+  });
+  
