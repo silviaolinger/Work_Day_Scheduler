cfeb9998112a013aa85e2ddc36c5676f69e9a6b0
sylvynhaarc@hotmail.com <you@example.com>
2022-11-16 22:10:14 -0600

    logic to display events saved in localStorage completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,75 +1,84 @@
 
 const viewcurrentDay = document.querySelector('#currentDay');
-const currentday= moment();
+const currentday = moment();
 viewcurrentDay.textContent = currentday.format('LLL');
 var currentHour = parseInt(moment().format("H"));
 console.log(currentHour)
 
 
-let timeBlock = $(".time-block").addClass('row');
-let description= $('.description').addClass('description');
+let timeBlock = $(".time-block");
+//console.log(timeBlock);
+let description = $('.description');
 
 //Saving Event
 
-    // On button click, get value
-    
-const savedevent= JSON.parse(localStorage.getItem('savedevent')) || [];
+// On button click, get value
+
+const savedevent = JSON.parse(localStorage.getItem('savedevent')) || [];
 $(document).ready(function () {
-    
+
     $(".saveBtn").click(function (event) {
         event.preventDefault()
 
-        var currentRow=$(this).closest("tr"); 
-         
-         const data ={
-          description: currentRow.find("td:eq(0) input[type='text']").val(),
-           time: currentRow.find("th:eq(0)").html(),
-        
+        var currentRow = $(this).closest("tr");
+
+        const data = {
+            description: currentRow.find("td:eq(0) input[type='text']").val(),
+            time: currentRow.find("th:eq(0)").html(),
+            id: currentRow.find("td:eq(0) input[id]").attr("id")
         }
-    
+
         savedevent.push(data)
-        localStorage.setItem('savedevent',JSON.stringify(savedevent));
-        
+        localStorage.setItem('savedevent', JSON.stringify(savedevent));
+
     });
 
 });
-//localStorage.clear();
 
-$( window ).on( "load", function() {
-   
-     
+
+$(window).on("load", function () {
+
+
     var events = JSON.parse(localStorage.getItem("savedevent"));
-     $.each(events,function (i,val){
-        var desc= $('<input desc="'+ val.description + '" />');
-           $('.description').append(desc);
+    console.log(events)
 
-        // $('.description')= desc
-     });
+    $(".description").each(function () {
+        var $this = $(this);
+        var id = parseInt($($this).attr("id"));
+        console.log(id)
+
+        for (var i = 0; i < events.length; i++) {
+            if (parseInt(events[i].id) === id) {
+                $this.val(events[i].description);
+            }
+
+        }
+
+    });
 });
-       
-   
+
 $(".table input[id]").each(function () {
     var $this = $(this);
-    var id = parseInt($($this).attr("id") );
-  
-  if (id < currentHour){
-  $(this).addClass("past");
-  $(this).removeClass("present")
-  $(this).removeClass("future")
-  }
-  
-  if (id > currentHour){
-  $(this).removeClass("past");
-  $(this).removeClass("present")
-  $(this).addClass("future")
-  }
-  
-  else if(id === currentHour) {
-  $(this).removeClass("past");
-  $(this).addClass("present")
-  $(this).removeClass("future")
-  }
-  
-  
-  });
-  
+    var id = parseInt($($this).attr("id"));
+
+    if (id < currentHour) {
+        $(this).addClass("past");
+        $(this).removeClass("present")
+        $(this).removeClass("future")
+    }
+
+    if (id > currentHour) {
+        $(this).removeClass("past");
+        $(this).removeClass("present")
+        $(this).addClass("future")
+    }
+
+    else if (id === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present")
+        $(this).removeClass("future")
+    }
+
+
+});
+
